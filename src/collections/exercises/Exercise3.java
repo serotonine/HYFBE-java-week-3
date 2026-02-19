0ddd394988b7a7377a37117c3ea4d33ffc984478
serotonine <ks.serotonine@gmail.com>
2026-02-19 14:07:08 +0100

    Week 3 : Collections corrections.

--- src/collections/exercises/Exercise3.java
+++ src/collections/exercises/Exercise3.java
@@ -10,17 +10,46 @@ import java.util.Map;
  4. Retrieve one number by name.
  5. Remove one entry.
 */
-public class Exercise3
-{
+public class Exercise3 {
     public static void main(String[] args) {
         Map<String, String> phoneBook = new HashMap<>();
-        phoneBook.put("Mom","+33561735680");
-        phoneBook.put("Rob","+32645668900");
-        phoneBook.put("Rob","+32645668900");
-        phoneBook.put("Cloclo","+33465789654");
-        System.out.println("Phonebook: " + phoneBook);
-        System.out.println("Get one element from phonebook: " + phoneBook.get("Mom"));
+        phoneBook.put("Mom", "+33561");
+        phoneBook.put("Rob", "+32645668900");
+        phoneBook.put("Rob", "+32645668900");
+        phoneBook.put("Cloclo", "+33465789654");
+        phoneBook.putIfAbsent("Mom", "+335456");
+        // System.out.println("Phonebook: " + phoneBook);
+        // System.out.println("Get one element from phonebook: " + phoneBook.get("Mom"));
         phoneBook.remove("Cloclo");
-        System.out.println("Phonebook after removal: " + phoneBook);
+        System.out.println("/// LOOPS ///");
+
+        phoneBook.computeIfPresent("Mom", (key, acc)-> acc += "734680");
+        phoneBook.computeIfAbsent("Cleopatre", key-> "00457896754");
+        phoneBook.put("Rob", "none");
+        printMap(phoneBook);
     }
+
+    public static void printMap(Map<String, String> aMap) {
+        System.out.println("Writing with keySet and enhanced loop");
+        for(String key : aMap.keySet()) {
+            System.out.print( aMap.get(key) + " ");
+        }
+        System.out.println();
+        System.out.println("Writing with entrySet and enhanced loop");
+        for(Map.Entry<String, String> entry : aMap.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
+        System.out.println("Writing with forEach and lambda function");
+        aMap.forEach((key, value) -> System.out.println(key + "'s phone number is: " + value));
+        System.out.println("Writing with toString method of Map");
+        System.out.println(aMap);
+        System.out.println("Writing the values");
+        for (String value : aMap.values()) {
+            System.out.println("value: " + value);
+        }
+    }
+
+
 }
+
+
--- src/collections/exercises/Exercise4.java
+++ src/collections/exercises/Exercise4.java
@@ -15,19 +15,25 @@ public class Exercise4
 {
     public static void main(String[] args) {
         Map<String, List<Integer>> grades = new HashMap<>();
-        grades.put("Germaine",Arrays.asList(45,55,78,23));
-        grades.put("Robert",Arrays.asList(45,13,51,12) );
-        grades.put("Pretti",Arrays.asList(75,86,78,65) );
-        grades.put("Fatima",Arrays.asList(95,55,90,67) );
-        grades.put("Momo",Arrays.asList(45,40,55,65) );
+        // Makes value mutable.
+        grades.put("Germaine",new ArrayList<>(Arrays.asList(45,55,78,23)));
+        grades.put("Robert",new ArrayList<>(Arrays.asList(45,13,51,12) ));
+        grades.put("Pretti",new ArrayList<>(Arrays.asList(75,86,78,65)) );
+        grades.put("Fatima",new ArrayList<>(Arrays.asList(95,55,90,67) ));
+        grades.put("Momo",new ArrayList<>(Arrays.asList(45,40,55,65) ));
+        grades.get("Momo").add(65);
         System.out.println(grades);
         printAverageGradeByStudent(grades);
     }
     public static void printAverageGradeByStudent(Map<String, List<Integer>> grades){
         grades.forEach((student, list)->{
-            int sum = list.stream().reduce(0, Integer::sum);
-            int average =  sum / list.size();
-            System.out.println(student +": "+average+"% success");
+            int total = 0;
+            for(int val:list){
+                total += val;
+            }
+            // int sum = list.stream().reduce(0, Integer::sum);
+            double average =  (double)(total / list.size());
+            System.out.println(student +": "+average+" % success");
         });
     }
 }
